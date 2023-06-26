6412fcccb2ef3dbcdd3d05e381b0e2041f48df23
floren <florendg@gmail.com>
2023-06-26 07:07:49 +0200

    solve warnings

--- src/services/greet.service.ts
+++ src/services/greet.service.ts
@@ -2,8 +2,7 @@ async function getHello(): Promise<JSON> {
     const response = await fetch('http://localhost:8080/api/hello');
     switch (response.status) {
         case 200:
-            const result = await response.json();
-            return result;
+            return await response.json();
         default:
             throw Error('unknown response status');
     }
@@ -13,8 +12,7 @@ async function getHelloName(name: string): Promise<JSON> {
     const response = await fetch(`http://localhost:8080/api/hello/${name}`);
     switch (response.status) {
         case 200:
-            const result = await response.json();
-            return result;
+            return await response.json();
         default:
             throw Error('unknown response status');
     }
